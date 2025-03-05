7c1ce825c7ef66aed3a9897ddf9b12c9e2c77be7
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-03-05 23:39:01 +0900

    Update nativize.js

--- nativize.js
+++ nativize.js
@@ -1,5 +1,36 @@
-export const check = async () => {};
-export const prepare = async () => {};
-export const build = async () => {};
-export const run = async () => {};
-export const clean = async () => {};
+export const check = async () => {
+	// check if cmake is installed
+	// check if qt is installed
+	//   we may need to get qt path...
+	//   if environment vars set
+	// check if qt webengine is installed
+};
+
+export const prepare = async () => {
+};
+
+export const build = async () => {
+	// cmake -B build -S .
+	await new Deno.Command("cmake", {
+		args: ["-B", "build", "-S", "."],
+		cwd: import.meta.dirname,
+	}).spawn().status;
+
+	// cmake --build build
+	await new Deno.Command("cmake", {
+		args: ["--build", "build"],
+		cwd: import.meta.dirname,
+	}).spawn().status;
+};
+
+export const run = async () => {
+	// run the app
+	// cmake does not have feature to run target
+	// we may either implement on CMakeLists.txt or
+	// here.
+};
+
+export const clean = async () => {
+	// clean up perhaps just delete build directory
+	await Deno.remove("build", { recursive: true });
+};
