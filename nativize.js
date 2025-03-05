7cbaad0316ba73e0aafaae89978f3a1e8fd8e398
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-03-05 12:38:24 +0900

    Create nativize.js

---
+++ nativize.js
@@ -0,0 +1,5 @@
+export const check = async () => {};
+export const prepare = async () => {};
+export const build = async () => {};
+export const run = async () => {};
+export const clean = async () => {};
