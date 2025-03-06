a8cf1a85d7f4215130f3f9d31029ebabed8ed58a
Garden Gim <gardengim@pm.me>
2025-03-06 11:08:26 +0900

    Implement Qt6 WebEngineWidgets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,13 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+    : QMainWindow(parent), webView(new QWebEngineView(this))
+{
+    setCentralWidget(webView);
+    webView->setUrl(QUrl("https://github.com/nativize"));
+}
 
-MainWindow::~MainWindow() {}
+MainWindow::~MainWindow()
+{
+    delete webView;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWebEngineView>
 
 class MainWindow : public QMainWindow
 {
@@ -10,5 +11,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    QWebEngineView *webView;
 };
+
 #endif // MAINWINDOW_H
